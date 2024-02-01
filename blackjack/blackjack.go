9190652c1f34cdbdb0986bd9e5071068d16f2cc8
Henk Jan Pluim <henkjan@kxdesign.nl>
2024-02-01 20:33:00 +0100

    wip

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -2,11 +2,65 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten":
+		return 10
+	case "jack":
+		return 10
+	case "king":
+		return 10
+	case "queen":
+		return 10
+	case "other":
+		return 0
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+	switch score := ParseCard(card1) + ParseCard(card2); {
+	case score >= 22:
+		return "P"
+	case score >= 21:
+		switch dealerscore := ParseCard(dealerCard); {
+		case dealerscore >= 10:
+			return "S"
+		default:
+			return "W"
+		}
+	case score >= 17 && score <= 20:
+		return "S"
+	case score >= 12 && score <= 16:
+		switch dealerscore := ParseCard(dealerCard); {
+		case dealerscore >= 7:
+			return "H"
+		default:
+			return "S"
+		}
+	case score <= 11:
+		return "H"
+	default:
+		return ""
+	}
 }
---
+++ need-for-speed/need_for_speed.go
@@ -0,0 +1,64 @@
+package speed
+
+import (
+	"fmt"
+)
+
+type Car struct {
+	battery      int
+	batteryDrain int
+	distance     int
+	speed        int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+		distance:     0,
+	}
+}
+
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	fmt.Println(car)
+	result := Car{
+		battery:      car.battery - car.batteryDrain,
+		batteryDrain: car.batteryDrain,
+		speed:        car.speed,
+		distance:     car.speed + car.distance,
+	}
+	switch batteryDist := car.battery - car.batteryDrain; {
+	case batteryDist < 0:
+		result.battery = car.battery
+		result.distance = car.distance
+		return result
+	default:
+		return result
+	}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	fmt.Println(car)
+	fmt.Println(track)
+	numberoftimes := car.battery / car.batteryDrain
+	fmt.Println(numberoftimes)
+	maxdistance := car.speed * numberoftimes
+	fmt.Println(maxdistance)
+	return maxdistance >= track.distance
+}
